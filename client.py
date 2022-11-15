8eb524442c6003f4b479803a890afb74571bbb97
omri gigi <gigiomri@gmail.com>
2022-11-15 20:16:56 +0200

    updating worker

--- client.py
+++ client.py
@@ -22,14 +22,23 @@ def Show():
     print('for update worker press 3')
     print('for view worker press 4')
     print('for view all workers press 5')  
-    print('for exit press 6')      
+    print('for exit press 6')
+    
+     
+def ShowUpdate():
+    print('for update worker press:')    
+    print('fristname:1\nlastname: 2\nage: 3\nid :4\nemail:5\nprofession :6\nsalary :7\nexperience: 8\n department 9\nexit 0')
+   
+   
 def workerdetails(workerid):
-    print("worker ",workerid,": { firstname: ",workers[workerid].firstname,"lastname: ",workers[workerid].lastname,"age: ",workers[workerid].age,"id: ",workers[workerid].id,"email: ",workers[workerid].email,"profession: ",workers[workerid].profession,"salary: ",workers[workerid].salary,"experience: ",workers[workerid].experience,"department: ",workers[workerid].department)
+    print("worker",workerid,": { firstname:",workers[workerid].firstname,"lastname:",workers[workerid].lastname,"age:",workers[workerid].age,"id:",workers[workerid].id,"email:",workers[workerid].email,"profession:",workers[workerid].profession,"salary:",workers[workerid].salary,"experience:",workers[workerid].experience,"department:",workers[workerid].department,"\n")
 
 
 def Exit():
     global isopen
     isopen = False
+    
+    
 def AddWorker():
     print("pls enter a firstname,lastname,age,id,email,profession,salary,experience,department with ,")
     #omri,gigi,22,000,mail,proggramer,65000,3,a
@@ -40,12 +49,49 @@ def AddWorker():
 
 
 def RemoveWorker():
-    print("RemoveWorker")
+    select=int(input("pls enter the workerid\n"))
+    workers.remove(workers[select])
+    
+    
 def UpdateWorker():
-    print("UpdateWorker")
+    updatemode = True
+    select = int(input("which worker you want to edit\n"))
+    while updatemode:
+        ShowUpdate()
+        selectupdate = int(input("which details you want to edit\n"))
+        if selectupdate != 0:
+            value =input("pls enter value\n")
+            match selectupdate: 
+                case 1:
+                    workers[select].firstname = value 
+                case 2:
+                        workers[select].lastname = value 
+                case 3:
+                        workers[select].age = value
+                case 4:
+                        workers[select].id = value
+                case 5:
+                        workers[select].email = value
+                case 6:
+                        workers[select].profession = value
+                case 7:
+                    workers[select].salary = value
+                case 8:
+                    workers[select].experience = value
+                case 9:
+                    workers[select].department = value  
+                case 0: 
+                    updatemode= False
+                case _:
+                    print("Worng number")
+        else:
+            updatemode=False
+            
+                        
 def ShowWorker():
-    select = int(input("which worker you want to see"))
+    select = int(input("which worker you want to see\n"))
     workerdetails(select)
+   
     
 def ShowAllWorker():
     for wokerid in range(len(workers)):
@@ -58,6 +104,8 @@ functions = {1:AddWorker,
              4:ShowWorker,
              5:ShowAllWorker,
              6:Exit }  
+
+
 def main():
     print('welcome to the ultimate worker system\n')
     while(isopen):
@@ -65,5 +113,7 @@ def main():
         select = int(input())
         functions[select]()
     print('the program stop')
+    
+    
 if __name__ =='__main__':
     main()
